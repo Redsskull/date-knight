02250b8b4ce28ae1f76739a4fd0abe8ddf4d0ef7
Redsskull <ivry91@gmail.com>
2024-02-15 09:10:29 +0100

    removed env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['CLOUDINARY_URL'] = 'CLOUDINARY_URL=cloudinary://913716536399478:-t8hVgl5Cg-pf9DA45Vl6vIgm7E@dluuak3rf'
-os.environ.setdefault("SECRET_KEY", '3D7C7F42CB7BBE47C3E549623D212')
-os.environ['DEV'] = '1'
-os.environ['DATABASE_URL'] = "postgres://ngppweiq:33VqqBf2o9NavQNqjnRcCtgQUxq8GBSw@bubble.db.elephantsql.com/ngppweiq"
