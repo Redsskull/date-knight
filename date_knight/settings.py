7d0fffe371bab5720a9365608654fa005c3687d3
Mika Virtucio <mikavirtucio@gmail.com>
2024-02-16 14:49:08 +0000

    Remove bin path for gitpod

--- date_knight/settings.py
+++ date_knight/settings.py
@@ -64,7 +64,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+# NPM_BIN_PATH = "/usr/local/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
