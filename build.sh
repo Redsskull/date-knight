fc30d2542d81bc2ed3359dfd368dd2d4b85a3a39
Redsskull <ivry91@gmail.com>
2024-02-17 13:15:45 +0100

    fixed build.sh for deployment

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 set -o errexit
 pip install -r requirements.txt
-python manage.py tailwind install
-python manage.py tailwind build
 python manage.py collectstatic --noinput
 python manage.py makemigrations && python manage.py migrate
 python manage.py createsuperuser --no-input
