657f28eedc030e596beb9dc0f74d32ebd0afb1b4
Redsskull <ivry91@gmail.com>
2024-02-17 22:06:17 +0100

    fixed collectstatic

--- date_knight/settings.py
+++ date_knight/settings.py
@@ -131,7 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'theme', 'static'),
-    os.path.join(BASE_DIR, 'static', 'date', 'images'),
 ]
 
 
