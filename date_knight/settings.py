2cea37cb4c19418b60e61d3ad8b9171d85c7f9bd
Redsskull <ivry91@gmail.com>
2024-02-17 21:34:35 +0100

    uodate STATICFILES_DIRS to include date images

--- date_knight/settings.py
+++ date_knight/settings.py
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'theme', 'static'),  # Add this line
+    os.path.join(BASE_DIR, 'theme', 'static'),
+    os.path.join(BASE_DIR, 'static', 'date', 'images'),
 ]
 
 
