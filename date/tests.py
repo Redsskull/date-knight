abd07e99ec5796df731a1ba3e3d4aac71c6afc54
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-02-18 15:32:25 +0000

    feat: Create test case "DateIdeaTestCase" for testing "DateIdea" model and create "test_date_idea()".

--- date/tests.py
+++ date/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from date.models import DateIdea
 
-# Create your tests here.
+
+class DateIdeaTestCase(TestCase):
+    def setUp(self):
+        DateIdea.objects.create(name="Test Idea", 
+                                description="This is a test idea",
+                                budget=1,
+                                place="city",
+                                time="day")
+
+    def test_date_idea(self):
+        test_idea = DateIdea.objects.get(name="Test Idea")
+        self.assertEqual(test_idea.description, "This is a test idea")
+        self.assertEqual(test_idea.budget, 1)
+        self.assertEqual(test_idea.place, "city")
+        self.assertEqual(test_idea.time, "day")
